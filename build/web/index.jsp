cd72dff5b345de07420b997f9c25c9e4fe726a1a
Anastasios Lelakis <tasos@hotmail.com>
2020-10-06 19:35:59 +0300

    added first jsps

---
+++ build/web/index.jsp
@@ -0,0 +1,59 @@
+<%-- 
+    Document   : index
+    Created on : Oct 6, 2020, 6:31:19 PM
+    Author     : user
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <div>Hello from my new Web application. This is my JSP page!</div>
+        <%--This is a scriptlet in my jsp page--%>
+        <%
+            printMessage(out);
+            for(int i=0;i<10;i++){
+                out.println("<br/>number:"+i);
+            }
+        %>
+        <ul>
+            <li>
+                <!--relative urls-->
+                <a href="MyServlet">MyServlet</a>
+            </li>
+            <li>
+                <a href="http://localhost:8080/WebServlet/SecondServlet?onoma=Jack&age=30">SecondServlet</a>
+            </li>
+            <li>
+                <a href="ListCustomerServlet">List of Customers (Servlet)</a>
+            </li>
+        </ul>
+
+        <%            
+            String footerMessage = "This is footer of JSP page";
+            out.print(footerMessage);
+        %>
+
+        <!--Declaration jsp tag-->
+        <%!
+            private java.time.LocalDateTime now = java.time.LocalDateTime.now();
+
+            private void printMessage(JspWriter out) {
+                int age = 20;
+                String message = "Hello from Scriptlet!!!";
+                message += " My age is " + age;
+                message += ". today is :" + now;
+                try {
+                    out.print(message);
+                } catch (Exception e) {
+
+                }
+            }
+        %>
+
+    </body>
+</html>
--- src/java/emergon/servlet/ListCustomerServlet.java
+++ src/java/emergon/servlet/ListCustomerServlet.java
@@ -4,6 +4,7 @@ import emergon.entity.Customer;
 import emergon.service.CustomerService;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -16,10 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 public class ListCustomerServlet extends HttpServlet {
 
     private CustomerService service = new CustomerService();
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    private LocalDateTime now = LocalDateTime.now();
+
+    private void myResponse(HttpServletResponse resp) throws IOException {
         List<Customer> listOfCustomers = service.getCustomers();
-        
+        System.out.println("ListCustomerServlet doGet method just called.....");
         StringBuilder builder = new StringBuilder();
         builder.append("<!DOCTYPE html>")
                 .append("<html>")
@@ -30,22 +32,31 @@ public class ListCustomerServlet extends HttpServlet {
                 .append("<h1>List of Customers</h1>")
                 .append("<a href='customer/insert'>").append("New Customer").append("</a>")
                 .append("<table border='1'>");
-        
-        for(Customer c: listOfCustomers){
+
+        for (Customer c : listOfCustomers) {
             builder.append("<tr>")
                     .append("<td>").append(c.getCcode()).append("</td>")
                     .append("<td>").append(c.getCname()).append("</td>")
                     .append("</tr>");
         }
-        
+
         builder.append("</table>")
                 .append("</body>")
                 .append("</html>");
-        
         PrintWriter writer = resp.getWriter();
         writer.print(builder.toString());
     }
 
-    
-   
+    @Override
+    public void init() throws ServletException {
+        System.out.println("ListCustomerServlet just Created!!!!!!!");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        myResponse(resp);
+
+    }
+
 }
